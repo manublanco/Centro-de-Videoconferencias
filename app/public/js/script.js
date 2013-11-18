a2320ee1a23efc3118279943f01d47451cb24bdf
manublanco <manublanco@ubuntu.(none)>
2013-11-18 17:09:48 +0100

    salas publicas finalizadas

--- app/public/js/script.js
+++ app/public/js/script.js
@@ -1,5 +1,6 @@
 var serverUrl = "/";
 var localStream, room, roomId, interval, rooms;
+var DEMO = {};
 
 var user = typeof(udata) != 'undefined' ? udata : { };
 
@@ -53,13 +54,21 @@ function getParameterByName(name) {
 
 
 
+
+
+
+
 window.onload = function () {
 
 try{
 
+    var usuario = $('#user-connected-tf').val();
+    var my_name = usuario;
+
+    var messText = document.getElementById('chat_message');
+    var chat_body = document.getElementById('chat_body');
     var screen = getParameterByName("screen");
-	localStream = Erizo.Stream({audio: true, video: true, data: true, screen: screen});
-//     localStream = Erizo.Stream({audio: true, video: true, data: true});
+	localStream = Erizo.Stream({audio: true, video: true, data: true, screen: screen, attributes:{name: my_name}});
 
 }catch (error) {
 
@@ -125,37 +134,60 @@ var displayUsers = function(userList) {
 getRooms (displayRooms);
 
 
-/*
-var writeUsers = function(id, users) {
-        var number;
-        if(users == '?') {
-            number = users; 
-        } else {
-            number = JSON.parse(users).length;
+//CHAT
+
+      messText.onkeyup = function(e) {
+          e = e || event;
+          if (e.keyCode === 13) {
+              DEMO.send_chat_message();
+          }
+          return true;
         }
 
-        room.childNodes[1].innerText = "Room " + id + " - (" + number + " users)";
+    var add_text_to_chat = function(text, style) {
+        var p = document.createElement('p');
+        p.className = 'chat_' + style;
+        p.innerHTML = text;
+        chat_body.appendChild(p);
+        chat_body.scrollTop = chat_body.scrollHeight;
+    }
+
+    DEMO.connect_to_chat = function() {
+    add_text_to_chat('Conexion a la sala con exito', 'italic');
+    }
+
+    DEMO.add_chat_participant = function(name) {
+        add_text_to_chat('Nuevo participante: ' + name, 'italic');
+    }
+
+     DEMO.remove_chat_participant = function(name) {
+        add_text_to_chat('Ha salido de la sala: ' + name, 'italic');
+    }
+
+    DEMO.send_chat_message = function() {
+        if(messText.value.match (/\S/)) {
+            if (localStream) {
+                localStream.sendData({msg: messText.value, name: my_name});
+            }
+            add_text_to_chat(my_name + ': ', 'name');
+            add_text_to_chat(messText.value, '');
+        }
+        messText.value = '';
     };
 
-    var checkUsers = function() {
-        getUsers(roomId1, function(users) {
-            writeUsers(1, users);
-            getUsers(roomId2, function(users) {
-                writeUsers(2,users);
-                getUsers(roomId3, function(users) {
-                    writeUsers(3,users);
-                    getUsers(roomId4, function(users) {
-                        writeUsers(4, users);
-                    });    
-                });
-            });
-        });
-    };
+    DEMO.chat_message_received = function(evt) {
+        var msg = evt.msg;
+        add_text_to_chat(msg.name + ': ', 'name');
+        add_text_to_chat(msg.msg, '');
+    }
 
-    interval = setInterval(checkUsers, 10000);
+    var connect_user = function () {
+        $('#connection_panel').modal('hide');
+        my_name = document.getElementById('username_txt').value;
+        DEMO.init_demo(my_name);
+    }
 
-    checkUsers();
-*/
+
 
 
     var checkUsers = function() {
@@ -218,8 +250,17 @@ var writeUsers = function(id, users) {
             room = Erizo.Room({token: token});
 
 
+         localStream.addEventListener("access-accepted", function () {
+              var subscribeToStreams = function (streams) {
+                for (var index in streams) {
+                  var stream = streams[index];
+                  if (localStream.getID() !== stream.getID()) {
+                    room.subscribe(stream);
+                  }
+                }
+              };
 
-
+/*
             localStream.addEventListener("access-accepted", function () {
                 
                 var subscribeToStreams = function (streams) {
@@ -237,10 +278,11 @@ var writeUsers = function(id, users) {
                             }
                         }
                     }
-                };
+                };*/
 
                 room.addEventListener("room-connected", function (roomEvent) {
                     // Publish my stream
+                    DEMO.connect_to_chat();
                     room.publish(localStream);
 
                     // Subscribe to other streams
@@ -249,7 +291,7 @@ var writeUsers = function(id, users) {
 
 
                 
-                
+                /*
                 room.addEventListener("stream-subscribed", function(streamEvent) {
                 var stream = streamEvent.stream;
                 var div = document.createElement('div');
@@ -259,7 +301,18 @@ var writeUsers = function(id, users) {
                 document.body.appendChild(div);
                 stream.show("test" + stream.getID());
 
-            });
+            });*/
+
+                room.addEventListener("stream-subscribed", function(streamEvent) {
+                    var stream = streamEvent.stream;
+
+                    add_div_to_grid("test" + stream.getID())
+                    stream.show("test" + stream.getID());
+
+                    stream.addEventListener("stream-data", DEMO.chat_message_received);
+                    DEMO.add_chat_participant(stream.getAttributes().name);
+
+                  });
 
 
                 room.addEventListener("stream-added", function (streamEvent) {
@@ -269,31 +322,98 @@ var writeUsers = function(id, users) {
                     subscribeToStreams(streams);
                 });
 
-                room.addEventListener("stream-removed", function (streamEvent) {
-                    // Remove stream from DOM
-                    var stream = streamEvent.stream;
-                    if (stream.elementID !== undefined) {
-                        console.log("Removing " + stream.elementID);
-                        var element = document.getElementById(stream.elementID);
-                        element.parentNode.removeChild(element);
-                    }
-                });
+
+
+              room.addEventListener("stream-removed", function (streamEvent) {
+                // Remove stream from DOM
+                var stream = streamEvent.stream;
+                if (stream.elementID !== undefined) {
+                  remove_div_from_grid(stream.elementID, "video_grid");
+                  DEMO.remove_chat_participant(stream.getAttributes().name);
+
+                }
+              });
 
                 room.connect();
 
-
-
-                localStream.show("myVideo");
+                add_div_to_grid("localVideo")
+                localStream.show("localVideo");
 
 
             });
             localStream.init();
         });  
             //var connected = getUsers(roomId,displayUsers);
-            interval=setInterval(checkUsers,10000);
-            getUsers(roomId,displayUsers);
+            //interval=setInterval(checkUsers,10000);
+            //getUsers(roomId,displayUsers);
     }
 
 };
+
+//VIDEO_GRID
+
+var add_div_to_grid = function(divId) {
+
+    $('#video_grid').css('border', 'none');
+
+    var grid = document.getElementById('video_grid');
+    var newDiv = document.createElement('div');
+    newDiv.setAttribute("id", divId + '_container');
+    newDiv.className = newDiv.className + " grid_element_border";
+
+    var newDiv2 = document.createElement('div');
+    newDiv2.setAttribute("id", divId);
+    newDiv2.className = newDiv2.className + " grid_element";
+    newDiv.appendChild(newDiv2);
+
+    grid.appendChild(newDiv);   
+    resizeGrid('video_grid');
+}
+
+var remove_div_from_grid = function(divId) {
+
+    var grid = document.getElementById('video_grid');
+    grid.removeChild(document.getElementById(divId + '_container'));
+    resizeGrid('video_grid');
+}
+
+var resizeGrid = function() {
+
+    var grid = document.getElementById('video_grid');
+    var nChilds = grid.childElementCount;
+
+    var c = Math.floor((nChilds-1)/3);
+    var r = (nChilds-1) % 3;
+
+    if (nChilds === 1) {
+        grid.childNodes[0].setAttribute("style","width: 100%; height: 100%;");
+    } else {
+
+        var height = 100/(c+1);
+        
+        for(var i = 1; i <= nChilds; i++) {
+
+            var row = Math.floor((i-1) / 3);
+            var width = 100/3;
+
+            if (r === 0) {  // las dos ultimas filas tienen dos videos
+
+                if (row > c - 2) { 
+                    width = 50;
+                }
+                grid.childNodes[i-1].setAttribute("style", "width: " + width + "%; height: " + height + "%;");
+
+            } else if (r === 1) {  // la ultima fila tiene un video
+                if (row === c) { 
+                    width = 50;
+                }
+                grid.childNodes[i-1].setAttribute("style", "width: " + width + "%; height: " + height + "%;");
+
+            } else {
+                grid.childNodes[i-1].setAttribute("style", "width: " + width + "%; height: " + height + "%;");
+            }
+        }
+    }
+} 
 
 
