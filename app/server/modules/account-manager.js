0e8dc5968cfa5fc81d5ad6c881319acc307bbec7
Manuel Blanco <manublanco@msn.com>
2013-12-06 00:03:18 +0100

    correccion mas  errores

--- app/server/modules/account-manager.js
+++ app/server/modules/account-manager.js
@@ -89,27 +89,7 @@ exports.addNewEvent = function(newEventData, callback)
 
 	accounts.findOne({user: newEventData.user},function(e,o){
 		
-		//events.insert(newEventData, {safe: true}, callback);
-
-		o.titulo 		= newEventData.titulo;
-		o.gestor 		= newEventData.gestor;
-		o.descripcion 	= newEventData.descripcion;
-		o.fecha			= newEventData.fecha;
-		o.hora			= newEventData.hora;
-		o.sala 			= newEventData.sala;
-
-		if (newData.invitados == ''){
-			events.save(o,{safe: true}, function(err){
-				if (err) callback(err);
-				else callback(null,o);
-			});
-		} else{
-			o.invitados = newEventData.invitados;
-			accounts.save(o,{safe: true}, function(err) {
-				if (err) callback(err);
-				else callback(null,o);
-			});
-		}
+		events.insert(newEventData, {safe: true}, callback);
 
 		
 	});	
